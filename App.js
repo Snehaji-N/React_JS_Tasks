ee7991b61df45b329b1166b4b46d0748488f0170
Snehaji N <143413796+Snehaji-N@users.noreply.github.com>
2023-10-16 23:30:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import WordCounter from './components/countWord';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/> 
+    </div>
+  );
+  }
+        /*
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+export default App;
